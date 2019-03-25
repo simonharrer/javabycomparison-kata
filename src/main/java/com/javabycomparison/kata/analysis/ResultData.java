678068a89aee251e98ddcbaf711178e44a0fcea1
Simon Harrer <simon.harrer@innoq.com>
2019-03-25 10:22:22 +0100

    WIP

--- src/main/java/com/javabycomparison/kata/analysis/ResultData.java
+++ src/main/java/com/javabycomparison/kata/analysis/ResultData.java
@@ -19,7 +19,7 @@ public class ResultData {
   // JC Structure JavaDoc of Constructors
   public ResultData(int type, String name, int LOC, int commentLOC, int numMethod, int nImports) {
     this.type = type;
-    this.name = name;
+    this.name = name.replaceAll("\\\\", "/");
     this.LOC = LOC;
     this.commentLOC = commentLOC;
     this.numMethod = numMethod;
